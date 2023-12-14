729e57e16c5824a8bc541b9e8aa90b6c170bd3db
berat584 <berat584@BPC08>
2023-12-14 13:38:33 +0300

    Proje dosyası ekle.

---
+++ ToggUretimi/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TOGGURETIMI
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Program başladı.");
+            Otomobil oto = new Otomobil();
+            Console.WriteLine("Program bitti.");
+        }
+    }
+    class Otomobil
+    {
+        string marka = "";
+        string renk = "";
+        public Otomobil()
+        {
+            marka = "TOGG";
+            renk = "PEMBE";
+            Console.WriteLine("yapıcı metot çalıştı");
+        }
+        ~Otomobil()
+        {
+            Console.WriteLine("nesne hafızadan atıldı");
+
+        }
+    }
+
+}
